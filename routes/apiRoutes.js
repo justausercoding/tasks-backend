9425bda87d8224a9e01265b0f5da574122525501
Pascal Rivoire <117156701+justausercoding@users.noreply.github.com>
2025-06-19 19:40:33 +0200

    Add controllers to their own files.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,12 @@
 import express from "express";
-import tasks from "../data/database.js";
-import { nanoid } from "nanoid";
+import { getAllTasks } from "../controllers/getAllTasks.js";
+import { addNewTask } from "../controllers/addNewTask.js";
+import { updateTask } from "../controllers/updateTask.js";
+import { deleteTask } from "../controllers/deleteTask.js";
 
 export const apiRouter = express.Router();
 
-apiRouter.get("/tasks", (req, res) => {
-  res.json(tasks);
-});
-
-apiRouter.post("/tasks", (req, res) => {
-  if (
-    !("name" in req.body) ||
-    !req.body.name ||
-    !("description" in req.body) ||
-    !("project" in req.body) ||
-    !("addedDate" in req.body) ||
-    !("dueDate" in req.body)
-  ) {
-    res.status(400).json({ message: "required fields are missing" });
-  }
-
-  const { name, description, project, addedDate, dueDate } = req.body;
-
-  const newTask = {
-    name,
-    description,
-    project,
-    dueDate,
-    addedDate,
-  };
-
-  newTask.id = nanoid();
-  newTask.done = false;
-  newTask.doneDate = null;
-
-  tasks.push(newTask);
-
-  res.status(201).json({ message: "task added successfuly" });
-});
+apiRouter.get("/tasks", getAllTasks);
+apiRouter.post("/tasks", addNewTask);
+apiRouter.patch("/tasks/:id", updateTask);
+apiRouter.delete("/tasks/:id", deleteTask);
