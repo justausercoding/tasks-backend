36d27d10dc9dd05439670f01326ea3e3dc35a2f6
Pascal Rivoire <117156701+justausercoding@users.noreply.github.com>
2025-06-26 19:38:27 +0200

    Remove fiels as required.

--- controllers/addNewTask.js
+++ controllers/addNewTask.js
@@ -2,18 +2,13 @@ import { data } from "../data/databaseInstance.js";
 
 export const addNewTask = (req, res) => {
   // check if data is all right
-  if (
-    !("name" in req.body) ||
-    !req.body.name ||
-    !("description" in req.body) ||
-    !("project" in req.body) ||
-    !("dueDate" in req.body)
-  ) {
+  if (!("name" in req.body) || !req.body.name) {
     return res.status(400).json({ message: "Required fields are missing" });
   }
 
   // add a new task to the database
-  const { name, description, project, dueDate } = req.body;
+  //  (if there is no descrition, project or dueDate value is 'null')
+  const { name, description = null, project = null, dueDate = null } = req.body;
 
   const result = data.addOneTask(name, description, project, dueDate);
 
