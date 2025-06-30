978eec9a62ecd3177d35a7cc92a52ee0d4055511
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-06-30 21:29:17 +0200

    Correct import

--- data/databaseInstance.js
+++ data/databaseInstance.js
@@ -1,5 +1,5 @@
 import initialTasks from "./initialTasks/initialTasks.js";
-import { DatabaseClass } from "./class/databaseClass.js";
+import { DatabaseClass } from "./class/DatabaseClass.js";
 
 // create an instance of the database which is then accessible from the controllers
 const data = new DatabaseClass(initialTasks);
