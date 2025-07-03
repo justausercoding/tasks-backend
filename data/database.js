0397287943abf5d5828ba7435d180b2f5aef7442
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-07-03 18:11:48 +0200

    Create database where all tasks are added

--- data/database.js
+++ data/database.js
@@ -1,7 +1,3 @@
-import initialTasks from "./initialTasks/initialTasks.js";
-import { DatabaseClass } from "./class/DatabaseClass.js";
+const database = {};
 
-// create an instance of the database which is then accessible from the controllers
-const data = new DatabaseClass(initialTasks);
-
-export { data };
+export { database };
