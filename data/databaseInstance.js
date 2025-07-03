a2cf871bda103662f77ba4596cb0fe18aa73515a
Pascal Rivoire <117156701+pass-r@users.noreply.github.com>
2025-07-03 18:11:56 +0200

    Delete databaseInstance.js

--- data/databaseInstance.js
+++
@@ -1,7 +0,0 @@
-import initialTasks from "./initialTasks/initialTasks.js";
-import { DatabaseClass } from "./class/DatabaseClass.js";
-
-// create an instance of the database which is then accessible from the controllers
-const data = new DatabaseClass(initialTasks);
-
-export { data };
